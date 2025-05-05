42c92d2e91fde6a651df1bd273fff87bcb9394ac
Klein <klein.gachuhi@student.moringaschool.com>
2025-05-05 22:48:02 +0300

    Comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-
 def return_evens(num_list):
+    evens = []
+    for n in num_list:
+        if n % 2 == 0:
+            evens.append(n)
+    return evens
+
     pass
 
+return_evens(range(10))
 def make_exclamation(sentence_list):
-    pass
+    result = []  
+    for sentence in sentence_list: 
+        result.append(sentence + "!") 
+    return result  
+
+make_exclamation(["I like computers", "I require coffee", "Live long and prosper"])
